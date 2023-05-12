d66a18f74a66d8509e17b60c6fc930cfe005482b
Robson Duarte <robosn.o.d@gmail.com>
2023-05-11 21:31:02 -0300

    :muscle:

--- bluetooth-classic/src/main.cpp
+++ bluetooth-classic/src/main.cpp
@@ -1,4 +1,39 @@
 #include "Arduino.h"
+#include "BluetoothSerial.h"
+#include "Preferences.h"
 
-void setup() {}
-void loop() {}
+
+String device_name = "ESP32 Bluetooth";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+Preferences preferences;
+
+String message = MESSAGE;
+
+void setup() {
+    preferences.begin("esp32", false);
+    preferences.putBool("ECHO", ECHO);
+
+    Serial.begin(115200);
+    SerialBT.begin(device_name);
+}
+
+
+
+void loop() {
+    while (SerialBT.available()){
+        message += String(SerialBT.read());
+    }
+    if(preferences.getBool("ECHO")){
+        SerialBT.println(message);
+    }
+    delay(1000);
+}
